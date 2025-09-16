79b0ad1526ed334148480819a046b6f41c0fe309
JoJo777890 <JoJo777890@gmail.com>
2025-09-16 23:42:59 +0800

    2025-9-16 Changed "triggerDistance" from 0.3f --> 0.04f -----||----- Switched from showing "3D Model" --> "Plane"

--- Assets/PlayerTokenTrigger.cs
+++ Assets/PlayerTokenTrigger.cs
@@ -6,7 +6,7 @@ using Vuforia;
 public class PlayerTokenTrigger : MonoBehaviour
 {
     public List<ObserverBehaviour> propertyTargets;
-    public float triggerDistance = 0.3f;
+    public float triggerDistance = 0.04f;
 
     private string PlayerLandedPropertyUIText;
 
@@ -35,11 +35,11 @@ public class PlayerTokenTrigger : MonoBehaviour
 
                 // Comment: Detection: 0.3f on "Y-axis" above overlap Player & Property
                 // Comment: "World Center Mode": "SPECIFIC" (Position used by Unity originally)
-                //float distance = Vector3.Distance(transform.position, property.transform.position + new Vector3(0f, 0.3f, 0f)); //之後改成 + new Vector3(0f, triggerDistance, 0f)
+                //float distance = Vector3.Distance(transform.position, property.transform.position + new Vector3(0f, triggerDistance, 0f)); 
 
                 // Comment: Detection: 0.3f on "Z-axis" above overlap Player & Property
                 // Comment: "World Center Mode": "DEVICE" (Postion used by Vuforia AR Camera)
-                float distance = Vector3.Distance(transform.position, property.transform.position + new Vector3(0f, 0f, 0.3f)); //之後改成 + new Vector3(0f, 0f, triggerDistance)
+                float distance = Vector3.Distance(transform.position, property.transform.position + new Vector3(0f, 0f, triggerDistance)); 
 
                 PlayerLandedPropertyUIText += $"Distance{distanceNum + 1}: {distance:F3}";
 
@@ -64,7 +64,7 @@ public class PlayerTokenTrigger : MonoBehaviour
 
         foreach (Transform child in property.transform)
         {
-            if (child.name.EndsWith("Model"))
+            if (child.name.EndsWith("Plane")) // Add ([Condition])([ || child.name.EndsWith("Model") ]) if you want 3D model to show up as well.
             {
                 child.gameObject.SetActive(true);
             }
@@ -78,7 +78,7 @@ public class PlayerTokenTrigger : MonoBehaviour
         {
             foreach (Transform child in property.transform)
             {
-                if (child.name.EndsWith("Model"))
+                if (child.name.EndsWith("Plane")) // Add ([Condition])([ || child.name.EndsWith("Model") ]) if you want 3D model to show up as well.
                 {
                     child.gameObject.SetActive(false);
                 }
