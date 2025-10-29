812b9e54b4f31b95ce7fdd4a0b3d83905178b799
JoJo777890 <JoJo777890@gmail.com>
2025-10-29 18:05:12 +0800

    2025-10-29 Added "BoardDefinition.cs"

---
+++ Assets/Scripts/ARMonopoly V4 - Full Scale V1/Board/BoardDefinition.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ARMonopolyV4-FullScale/BoardDefinition")]
+public class BoardDefinition : ScriptableObject
+{
+    public int goIndex = 0;
+    public int jailIndex = 10;
+    public BoardSpace[] spaces; // 0..N-1 clockwise
+}
+
+[Serializable]
+public struct BoardSpace
+{
+    public string id;            // unique, e.g., "TOKYO_TOWER"
+    public string displayName;   // localized key ok
+    public SpaceType type;
+    public string propertyId;    // filled if SpaceType.Property
+}
+
+public enum SpaceType 
+{
+    Start, 
+    Property
+}
+
