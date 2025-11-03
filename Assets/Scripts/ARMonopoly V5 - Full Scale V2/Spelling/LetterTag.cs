6cb284bc9c2e294cf0bd849dfb28088a97a204c9
JoJo777890 <JoJo777890@gmail.com>
2025-11-04 03:13:31 +0800

    2025-11-4 Added "LetterTag.cs"

---
+++ Assets/Scripts/ARMonopoly V5 - Full Scale V2/Spelling/LetterTag.cs
@@ -0,0 +1,14 @@
+// In folder: ARMonopoly V5 - Full Scale V2/Spelling/
+using UnityEngine;
+
+namespace ARMonopoly_V5___Full_Scale_V2.Spelling
+{
+    /// <summary>
+    /// Attach this to your letter Image Targets (A, B, C...)
+    /// to identify them as spelling letters.
+    /// </summary>
+    public class LetterTag : MonoBehaviour
+    {
+        public char Letter;
+    }
+}
